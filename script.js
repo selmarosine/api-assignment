f8c04b9934e6bc641b01dc5c33f08d722c8376b0
selmarosine <selma.gronsten@hotmail.se>
2023-12-02 18:33:31 +0100

    minimala ändringar, vill bara se så att git är kopplat

---
+++ script.js
@@ -0,0 +1,12 @@
+//fetch('https://api.thecatapi.com/v1/images/search')
+//.then (res => res.json())
+//.then(data => console.log(data))
+
+// detta behövs inte?
+//const katter =
+//new Request('https://http.cat/[status_code]', {
+  //  method: 'GET'
+//})
+//const response = fetch(katter)
+
+const katter = fetch('https://http.cat/[status_code]');
