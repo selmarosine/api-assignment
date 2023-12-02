781b18f11c6b09a25d7b652aaf62339b1a2b9d0d
selmarosine <selma.gronsten@hotmail.se>
2023-12-02 19:02:30 +0100

    nytt api, som funkar!

---
+++ script.js
@@ -0,0 +1,25 @@
+function fetchCats(apiKey) {
+    const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+
+    fetch(apiUrl, {
+        headers: {
+            'x-api-key': apiKey,
+        },
+    })
+    .then(Response => {
+        if(!Response.ok) {
+            throw new Error('Ett fel uppstod');
+        }
+        return Response.json();
+    })
+    .then(data => {
+        const catImageUrl = data[0].url;
+        document.getElementById('catImage').src = catImageUrl;
+    })
+    .catch (error => {
+        console.error('Error:', error);
+    });
+}
+
+const apiKey = 'live_tJPgGwszXakjsu3btk73VsiX6raq5Cp5gqA8PCjyGSp0xeSSCRzhzcE3OnLCXueK';
+fetchCats(apiKey);
